0f0f7156b7eed6510371c7e63791c15c6eeab809
Pievisco89 <pie-89@live.it>
2021-04-21 17:44:54 +0200

    script.js quasi finito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("ciaociaociaociao")
+$(function(){
+
+  $('.with-dropdown').mouseenter(function(){
+
+    $(this).find('.dropdown-menu').addClass('active');
+    
+  });
+
+  $('.with-dropdown').click(function(){
+
+    $(this).find('.dropdown-menu.active').removeClass('active');
+
+  });
+
+})
