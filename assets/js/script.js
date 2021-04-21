f4aea24d328bae1afa016bbd9347e19ec9465cf1
Pievisco89 <pie-89@live.it>
2021-04-21 22:38:12 +0200

    sistemato css, commenti script e coming soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 $(function(){
 
   $('.with-dropdown').mouseenter(function(){
-
+    /* punto i dropdown-menu con classe active e la tolgo */
     $('.dropdown-menu.active').removeClass('active');
-    
+    /* aggiungo al dropdown cliccato la classe active */
     $(this).find('.dropdown-menu').addClass('active');
     
   });
 
   $('.with-dropdown').click(function(){
-
+    /* cliccando sul link del menu tolgo active per chiudere il dropdown */
     $(this).find('.dropdown-menu.active').removeClass('active');
 
   });
